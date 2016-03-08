289dc425660cfa7fd1271ad041e7817cc08ca9f1
Mansi Jain <jain.ma@husky.neu.edu>
2016-03-08 16:26:31 -0500

    3

---
+++ AngularJS/js/exam3.js
@@ -0,0 +1,15 @@
+var app3 = angular.module('app3', []);
+
+// Define the Controller and implement the Scope
+app3.controller('gListCtrl', function($scope) {
+
+  $scope.groceries = [
+    {item: "Tomatoes", purchased: false},
+    {item: "Potatoes", purchased: false},
+    {item: "Bread", purchased: false},
+    {item: "Hummus", purchased: false}
+  ];
+
+
+
+});
